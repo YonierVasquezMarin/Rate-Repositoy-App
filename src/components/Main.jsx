fb96d5934f2ff259b4b7389fe3d0e8d13a1919d2
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-07-06 15:20:07 -0500

    Margen aplicada como constante de expo

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,11 @@
 import { View } from 'react-native'
 import React from 'react'
 import RepositoryList from './RepositoryList'
+import Constants from 'expo-constants'
 
 export default function Main() {
     return (
-        <View>
+        <View style={{marginTop: Constants.statusBarHeight}}>
             <RepositoryList />
         </View>
     )
