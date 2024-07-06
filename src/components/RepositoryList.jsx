632854de14a398f70dcb3851f029faafafb979e4
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-07-06 16:32:46 -0500

    Lista de items actualizada

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,22 +1,14 @@
-import { View, Text } from 'react-native'
+import { View, FlatList } from 'react-native'
 import React from 'react'
 import repositories from '../data/repositories'
+import RepositoryItem from './RepositoryItem'
 
 export default function RepositoryList() {
     return (
-        <View>
-            {repositories.map((repo) => (
-                <View key={repo.id}>
-                    <Text>ID: {repo.id}</Text>
-                    <Text>Fullname: {repo.fullName}</Text>
-                    <Text>Description: {repo.description}</Text>
-                    <Text>Language: {repo.language}</Text>
-                    <Text>Stars: {repo.stargazersCount}</Text>
-                    <Text>Forks: {repo.forksCount}</Text>
-                    <Text>Reviews: {repo.reviewCount}</Text>
-                    <Text>Rating: {repo.rating}</Text>
-                </View>
-            ))}
-        </View>
+        <FlatList
+            data={repositories}
+            ItemSeparatorComponent={() => <View style={{ height: 10 }}></View>}
+            renderItem={({ item }) => <RepositoryItem {...item}></RepositoryItem>}
+        ></FlatList>
     )
 }
