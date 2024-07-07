674d06f336f1c06a6dd1092a861dd3dd49498009
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-07-06 20:53:59 -0500

    Temas agregados

---
+++ src/theme.js
@@ -0,0 +1,18 @@
+export default theme = {
+    colors: {
+        textPrimary: 'grey',
+        textSecondary: 'blue',
+        primary: 'blue',
+    },
+    fontSizes: {
+        body: 14,
+        subheading: 16,
+    },
+    fonts: {
+        main: 'System',
+    },
+    fontWeights: {
+        normal: '400',
+        bold: '700',
+    },
+}
